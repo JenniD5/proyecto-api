dacc88dd369823bc2270a95dda0b33969d8e149f
JenniD5 <elizabethduran0105@gmail.com>
2019-09-24 13:38:26 -0700

    3

--- app-j.js
+++ app-j.js
@@ -4,7 +4,7 @@ const cat = document.getElementById('cat');
 const animalesurl=`http://shibe.online/api/`;
 
 let getAdopcionData = async ()=>{
-    const respose = await fetch (`${animalesurl}cats/ba5d49879ba0536ff531331b18eb94ec8e29c377.jpg`);
+    const respose = await fetch (`${animalesurl}cats`);
     const gatos = await respose.json();
     console.log(gatos);
     catsImg.scr =gatos;
